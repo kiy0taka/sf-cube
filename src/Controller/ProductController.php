26fc93d924adc0fbea427fb63a6d465c5a5195c5
kiy0taka <kiy0taka333@gmail.com>
2017-12-07 11:34:15 +0900

    save new Product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -11,11 +12,30 @@ use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
     /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+
+    /**
+     * ProductController constructor.
+     */
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    /**
      * @Route("/product/{id}", name="product")
      * @Template("product.twig")
      */
     public function index(Product $Product)
     {
+        $NewProduct = new Product();
+        $NewProduct->name = "ディナーフォーク";
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($NewProduct);
+        $em->flush();
+
         return ['Product' => $Product];
     }
 }
