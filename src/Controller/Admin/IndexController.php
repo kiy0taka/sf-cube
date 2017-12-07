2b590b12146b2779556bf909e6fc55b26f284470
kiy0taka <kiy0taka333@gmail.com>
2017-12-07 14:35:03 +0900

    login form

--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -5,11 +5,12 @@ namespace App\Controller\Admin;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller
 {
     /**
-     * @Route("/admin", name="admin_homepage")
+     * @Route("/admin/", name="admin_homepage")
      * @Template("@admin/index.twig")
      *
      */
@@ -17,4 +18,13 @@ class IndexController extends Controller
     {
         return [];
     }
+
+    /**
+     * @Route("/admin/login", name="admin_login")
+     */
+    public function login()
+    {
+        return new Response("Hello Login Form");
+    }
 }
+
