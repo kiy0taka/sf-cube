b0baf36d4070ce28dda524b5d3f001379922efd9
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2017-12-07 11:18:28 +0900

    bin/console doctrine:schema:create

---
+++ src/Entity/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    public $name;
+    // add your own fields
+}
