0eb06a7a25f87bacdcb43720ec14da775544b2a8
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2017-12-07 15:50:27 +0900

    add login form

--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -2,13 +2,31 @@
 
 namespace App\Controller\Admin;
 
+use App\Form\Admin\LoginType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller
 {
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * IndexController constructor.
+     *
+     * @param FormFactoryInterface $formFactory
+     */
+    public function __construct(FormFactoryInterface $formFactory)
+    {
+        $this->formFactory = $formFactory;
+    }
+
     /**
      * @Route("/admin/", name="admin_homepage")
      * @Template("@admin/index.twig")
@@ -21,10 +39,16 @@ class IndexController extends Controller
 
     /**
      * @Route("/admin/login", name="admin_login")
+     * @Template("@admin/login.twig")
      */
-    public function login()
+    public function login(Request $request)
     {
-        return new Response("Hello Login Form");
+        $builder = $this->formFactory->createBuilder(LoginType::class);
+        $form = $builder->getForm();
+        $form->handleRequest($request);
+
+        
+        return [];
     }
 }
 
